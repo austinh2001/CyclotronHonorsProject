7544513dbc442c59a67a3aafced5aeea7f55360e
austinh2001 <austinh2001@gmail.com>
2021-03-24 20:31:56 -0400

    Commented CoordinateSystem

--- Physics/CoordinateSystem.py
+++ Physics/CoordinateSystem.py
@@ -1,8 +1,16 @@
+#Imports
 import pygame
 from Physics.Vector import Vector
+
 class CoordinateSystem:
     def __init__(self,pixel_origin, conversionFactor, conversionType, pixel_screen_size = [800,600]):
+        #Origin of the coordinate system (in pixels), in terms of the location on the window you are using.
         self.pixel_origin = pixel_origin
+        
+        #The type of conversion that your coordinate system is utilizing
+        #By default, there exists two types of conversion types:
+        #PPM (Pixels Per Meter)
+        #MPP (Meters Per Pixel)
         self.conversionType = conversionType
         self.PPM = None
         self.MPP = None
@@ -12,9 +20,13 @@ class CoordinateSystem:
             self.MPP = conversionFactor
         else:
             raise Exception
+        
+        #The dimensions of the screen/window you are using (in pixels)
         self.pixel_screen_size = pixel_screen_size
         self.pixel_width = self.pixel_screen_size[0]
         self.pixel_height = self.pixel_screen_size[1]
+        
+        #The dimensions of the screen/window you are using (in meters)
         self.converted_screen_size = []
         if (self.conversionType == "PPM"):
             self.converted_screen_size.append(self.pixel_width / self.PPM)
@@ -22,7 +34,8 @@ class CoordinateSystem:
         elif(self.conversionType == "MPP"):
             self.converted_screen_size.append(self.pixel_width * self.MPP)
             self.converted_screen_size.append(self.pixel_height * self.MPP)
-
+    
+    #Takes a vector object (which contains pixel information) and converts it accordingly to a meter-based position vector
     def convertFromPixelVector(self,pixel_vector):
         if (self.conversionType == "PPM"):
             vector = (pixel_vector * (1 / self.PPM))
@@ -30,7 +43,8 @@ class CoordinateSystem:
             vector = pixel_vector * self.MPP
         vector = vector.rotateDegrees(90)
         return vector
-
+    
+    #Takes a vector object (which contains meter information) and converts it accordingly to a pixel-based position vector
     def convertToPixelPositionVector(self,vector):
         if (self.conversionType == "PPM"):
             pixel_vector = (vector * self.PPM)
